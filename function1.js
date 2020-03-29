2bd5c139d75d8cef1d54b3dbebe023a901feda59
Marie Serykh <marie.serykh@gmail.com>
2020-03-29 15:36:36 +0200

    Initial commit

---
+++ function1.js
@@ -0,0 +1,19 @@
+//food prices
+var beer_cost = 6.25;
+var burger_cost = 4.50;
+var pop_cost = 2.00;
+
+//user's money
+var account_balance = 15.75;
+
+//drink beer
+account_balance = account_balance - beer_cost;
+console.log(account_balance);
+
+//eat burger
+account_balance = account_balance - burger_cost;
+console.log(account_balance);
+
+//drink pop
+account_balance = account_balance - pop_cost;
+console.log(account_balance);
