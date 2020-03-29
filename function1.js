d082d413d1a5c42994feadf76eb34dfa0c99dd42
Marie Serykh <marie.serykh@gmail.com>
2020-03-29 15:46:24 +0200

    Declared and called functions

--- function1.js
+++ function1.js
@@ -1,19 +1,34 @@
 //food prices
 var beer_cost = 6.25;
-var burger_cost = 4.50;
-var pop_cost = 2.00;
+var burger_cost = 4.75;
+var pop_cost = 3.00;
 
 //user's money
 var account_balance = 15.75;
 
 //drink beer
-account_balance = account_balance - beer_cost;
-console.log(account_balance);
+function drink_beer() {
+    account_balance = account_balance - beer_cost;
+    console.log(account_balance);
+}
 
 //eat burger
-account_balance = account_balance - burger_cost;
-console.log(account_balance);
+function eat_burger() {
+    account_balance = account_balance - burger_cost;
+    console.log(account_balance);
+}
 
 //drink pop
-account_balance = account_balance - pop_cost;
-console.log(account_balance);
+function drink_pop() {
+    account_balance = account_balance - pop_cost;
+    console.log(account_balance);
+}
+
+drink_beer();
+drink_pop();
+drink_pop();
+eat_burger();
+drink_beer();
+drink_pop();
+drink_pop();
+eat_burger();
