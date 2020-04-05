780ad4da70aa8d996362bd18fcbbed1c67829438
Marie Serykh <marie.serykh@gmail.com>
2020-04-05 16:27:51 +0200

    * Added a filter by a simple function and an arrow function

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,3 +4,19 @@
 // parentheses around input if more than one
 // curly braces around function body if more than one line/command
 // return only needed if more than one line of code
+
+let numbers = [123, 234, 345, 456, 567];
+let names = ['Alex', 'Bree', 'Cara', 'Cole', 'Devon', 'Riley'];
+
+//simple function syntax
+//create a new array if the values from the array greater than 300
+
+let big = numbers.filter(function(item) {
+    return item > 300;
+});
+
+//arrow function version
+let bigA = numbers.filter( item => item > 300);
+
+console.log(big);
+console.log(bigA);
