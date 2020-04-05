f83e0b42a570ed2ce1f4de0ce6c74c75221964f9
Marie Serykh <marie.serykh@gmail.com>
2020-04-05 16:47:07 +0200

    Learned 3 options * one parameter * no parameters * returning objects

--- arrow-functions.js
+++ arrow-functions.js
@@ -32,9 +32,22 @@ let retVal = 42;
 let obj = {'prop1': 'foo', 'prop2': 'bar'};
 
 //1. One parameter, with and without curly braces
-const f1 = (param) => param;
+const f1 = (param) => {return param};
+console.log( f1(7) );
 const f2 = param => param;
+console.log( f2(7) );
 
 //2. No parameters
+const f3 = () => retVal;
+console.log( f3() );
+const f4 = _ => retVal;
+console.log( f4() );
 
 //3. Returning objects
+const f5 = (param) => obj;
+//const f6 (param) => {'prop1': 'foo', 'prop2': 'bar'};
+//fails
+console.log( f5() );
+const f7 = _ => ({'prop1': 'foo',
+                  'prop2': 'bar'});
+console.log( f7() );
