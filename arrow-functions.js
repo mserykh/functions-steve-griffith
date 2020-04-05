8c1fef8f36f2b51ae422ce54a23e7dbe43fa0179
Marie Serykh <marie.serykh@gmail.com>
2020-04-05 16:17:19 +0200

    Created a file * Added a description

---
+++ arrow-functions.js
@@ -0,0 +1,6 @@
+
+// ES6 (fat) Arrow functions
+//work best in callback function scenarios
+// parentheses around input if more than one
+// curly braces around function body if more than one line/command
+// return only needed if more than one line of code
